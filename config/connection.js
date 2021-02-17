5328b2e530d04d3ec736c0f610945d5bad55598e
Anya L. Keller <kelleranyal@gmail.com>
2021-02-16 22:13:58 -0500

    setup for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 // Set up MySQL connection.
-const mysql = require('mysql');
+const mysql = require("mysql");
+var connection;
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: process.env.DB_USER,
-  // NOTE: Be sure to add your MySQL password here!
-  password: process.env.DB_PASS,
-  database: 'cat_db',
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    // NOTE: Be sure to add your MySQL password here!
+    password: "ur_password",
+    database: "cat_db",
+  });
+}
 
 // Make connection.
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
     return;
